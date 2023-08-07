bd0af449c519092aa3ca3218f351f7632ea2ef95
Kiran Anand <liteninkiran@hotmail.com>
2023-08-07 22:41:45 +0100

    Add data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ApiService } from './services/api.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'test-project';
+export class AppComponent implements OnInit, OnDestroy {
+    public covid$: Observable<any>;
+    public covidSub: Subscription;
+
+    constructor(private api: ApiService) {}
+
+    public ngOnInit(): void {
+        this.covid$ = this.api.getCovidData();
+        this.covidSub = this.covid$.subscribe((data: any) => {
+            console.log(data);
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.covidSub.unsubscribe();
+    }
 }
