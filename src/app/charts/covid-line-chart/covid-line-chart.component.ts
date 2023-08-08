8f49e1bc40556f042f58c5da12dd499b83e25ebc
Kiran Anand <liteninkiran@hotmail.com>
2023-08-08 17:09:18 +0100

    Add properties and methods

--- src/app/charts/covid-line-chart/covid-line-chart.component.ts
+++ src/app/charts/covid-line-chart/covid-line-chart.component.ts
@@ -1,21 +1,111 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ElementRef, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
     selector: 'app-covid-line-chart',
     templateUrl: './covid-line-chart.component.html',
     styleUrls: ['./covid-line-chart.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
-export class CovidLineChartComponent implements OnInit {
+export class CovidLineChartComponent implements OnInit, OnChanges {
 
     @Input() public data: any;
 
-    constructor() {}
+    // Main elements
+    public host: any;
+    public svg: any;
+
+    // Dimensions
+    public dimensions: DOMRect;
+    public innerWidth: number;
+    public innerHeight: number;
+    public margins = {
+        left: 50,
+        top: 40,
+        right: 20,
+        bottom: 80,
+    };
+
+    // Containers
+    public dataContainer: any;
+    public xAxisContainer: any;
+    public yAxisContainer: any;
+    public legendContainer: any;
+    public title: any;
+
+    // Scales
+    public x: any;
+    public y: any;
+    public colours: any;
+
+    // Axes
+    public xAxis: any;
+    public yAxis: any;
+
+    // Line generator
+    public line: any;
+
+    constructor(element: ElementRef) {
+        this.host = d3.select(element.nativeElement);
+    }
 
     public ngOnInit(): void {
+        this.svg = this.host.select('svg');
+        this.setDimensions();
+        this.setElements();
+        this.updateChart();
+        console.log(this);
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (this.svg) {
+            this.updateChart();
+        }
     }
 
     public onClick(): void {
         console.log(this.data);
+    }
+
+    private updateChart(): void {
+        if (this.data) {
+            this.setParams();
+            this.setLabels();
+            this.setAxis();
+            this.setLegend();
+            this.draw();
+        }
+    }
+
+    private setDimensions(): void {
+        this.dimensions = this.svg.node().getBoundingClientRect();
+        this.innerWidth = this.dimensions.width - this.margins.left - this.margins.right;
+        this.innerHeight = this.dimensions.height - this.margins.top - this.margins.bottom;
+        this.svg.attr('viewBox', [0, 0, this.dimensions.width, this.dimensions.height]);
+    }
+
+    private setElements(): void {
+
+    }
+
+    private setParams(): void {
+
+    }
+
+    private setLabels(): void {
+
+    }
+
+    private setAxis(): void {
+
+    }
+
+    private setLegend(): void {
+
+    }
+
+    private draw(): void {
+
     }
 
 }
