2e1ab982a4e751ab82804ee6f6652c15b06aa9da
Kiran Anand <liteninkiran@hotmail.com>
2023-08-09 17:10:22 +0100

    Scaling the bars

--- src/app/charts/my-first-chart/my-first-chart.component.ts
+++ src/app/charts/my-first-chart/my-first-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -9,13 +9,29 @@ import * as d3 from 'd3';
 export class MyFirstChartComponent implements OnInit {
 
     public data = [125, 100, 50, 75, 200];
+    public width: number;
+    public max: number;
 
-    constructor() {
+    // Main elements
+    public host: any;
+    public svg: any;
 
+    // Dimensions
+    public dimensions: DOMRect;
+
+    constructor(element: ElementRef) {
+        this.host = d3.select(element.nativeElement);
     }
 
     public ngOnInit(): void {
-
+        this.svg = this.host.select('svg');
+        this.setDimensions();
     }
 
+    private setDimensions(): void {
+        this.dimensions = this.svg.node().getBoundingClientRect();
+        this.width = this.dimensions.width / this.data.length;
+        this.max = Math.max(...this.data);
+        this.svg.attr('viewBox', [0, 0, this.dimensions.width, this.dimensions.height]);
+    }
 }
